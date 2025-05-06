87704a8d77485cba83ee22a7343aba00a0806895
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-05-06 14:59:54 +0300

    done!

--- src/main/java/com/example/brodr/BroDrApplication.java
+++ src/main/java/com/example/brodr/BroDrApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BroDrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BroDrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BroDrApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/brodr/Home.java
+++ src/main/java/com/example/brodr/Home.java
@@ -1,4 +1,13 @@
 package com.example.brodr;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Home {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
 }
